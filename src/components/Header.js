bcd3f0a5b898e2396099f53fd849e16a41fb44ab
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2024-02-19 02:05:52 -0600

    Add buttons - test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import '../index.css'
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    let navigate = useNavigate();
+
     return (
         <header>
             <div class="bg-green-500 py-4 grid justify-items-center">
                 <h1 class="text-3xl font-bold text-white">Industrial Supply & Service</h1>
             </div>
             <nav class="bg-gray-900">
+                <ul class="flex justify-center">
+                    <button onClick={() => navigate("/Home")}>Home</button>
+                    <button onClick={() => navigate("/About")}>About</button>
+                </ul>
             </nav>
         </header>
     )
